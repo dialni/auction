71e3a488f88e2763290ec39e6ea73d213886677a
= <alfredsoldin@gmail.com>
2025-11-24 18:03:11 +0100

    fmcl

--- Client/Client.go
+++ Client/Client.go
@@ -1,14 +1,90 @@
-﻿package Client
+﻿package main
 
-// if connection drops before requests completes, set port to +1 and send request again
+import (
+	p "auction/protoc"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
 
-type client struct {
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+// todo: change log messages to correct form
+// ListenForMessages is mostly self-explanitory, it listens are parses messages from the server to the clients terminal
+func ListenForMessages(stream grpc.BidiStreamingClient[p.Message, p.Result]) {
+	for {
+		msg, err := stream.Recv()
+		if err != nil {
+			log.Fatalf("[CLIENT]: Lost connection to server.")
+		}
+
+	}
 }
 
-func bid(int) {
+func main() {
+	conn, err := grpc.NewClient(":8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
 
+	c := p.NewAuctionServiceClient(conn)
+
+	stream, err := c.AuctionStream(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Start listening for messages in the background.
+	go ListenForMessages(stream)
+	fmt.Println("Enter a username: ")
+	in := bufio.NewScanner(os.Stdin)
+	in.Scan()
+	username := in.Text()
+
+	//Start listening for commands
+	for {
+		in.Scan()
+		switch in.Text() {
+		case "Bid":
+			log.Println("State your bid!")
+			for {
+				in.Scan()
+				bid, err := strconv.Atoi(in.Text())
+				if err != nil {
+					break
+				}
+				Bid(int32(bid), username)
+			}
+			break
+
+		case "Result":
+			Result()
+			break
+		}
+	}
 }
 
-func result() {
+func Bid(bid int32, username string) {
+	var msg p.Message
+	msg = p.Message{
+		UserID: username,
+		IsBid:  true,
+		Price:  bid,
+	}
+	stream.send(&msg)
+}
 
+func Result() {
+	var msg p.Message
+	msg = p.Message{
+		UserID: "",
+		IsBid:  false,
+		Price:  0,
+	}
+	stream.send(&msg)
 }
--- Server/Server.go
+++ Server/Server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	p "auction/protoc"
+	"fmt"
 	"log"
 	"net"
 	"sync"
@@ -17,6 +18,7 @@ const (
 
 type Server struct {
 	p.UnimplementedAuctionServiceServer
+	ServerPort int
 	//clients map[int]*grpc.ServerStream // måske useless, hvis der ikke er en grund til at holde øje med dem
 	servers        map[int]*p.AuctionService_AuctionStreamClient // holder øje med om servers er nede
 	CurrentAuction Auction
@@ -24,10 +26,10 @@ type Server struct {
 
 type Auction struct {
 	sync.Mutex
-	TopBidder  int   // port of highest bidder
-	HighestBid int32 // current highest bid
-	TimeLeft   int64 // time left before auction ends
-	IsDone     bool  // if auction is done
+	TopBidder  string // port of highest bidder
+	HighestBid int32  // current highest bid
+	TimeLeft   int64  // time left before auction ends
+	IsDone     bool   // if auction is done
 }
 
 type BrokerMessage struct {
@@ -36,25 +38,52 @@ type BrokerMessage struct {
 func main() {
 	s := &Server{}
 
-	lis, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-
 	grpcServer := grpc.NewServer()
+	var lis net.Listener
+	var err error
+
+	// Dynamically assign services to ports from 5001 to 5003 (3 active nodes, + critical zone on 5000)
+	for i := 5000; i < 5004; i++ {
+		s.ServerPort++
+		lis, err = net.Listen("tcp", fmt.Sprintf(":%d", s.ServerPort))
+		if err != nil {
+			log.Println("Port ", s.ServerPort, " in use, trying next port in line...")
+			continue
+		} else {
+			break
+		}
+	}
+	for i := 5001; i < 5004; i++ {
+		//if int32(i) != s.ServerPort {
+		//s.clients = append(s.clients, int32(i))
+		//}
+	}
+	//log.Println("My list of contacts:", s.clients)
+	log.Printf("Server has started, listening at %v", lis.Addr())
 	p.RegisterAuctionServiceServer(grpcServer, s)
-	if err := grpcServer.Serve(lis); err != nil {
+
+	go s.StartAuction()
+
+	if err = grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
 
 func (s *Server) StartAuction() {
 	var auctionTime int64 = 60
-	s.CurrentAuction = Auction{TopBidder: 0, HighestBid: 10, TimeLeft: time.Now().Unix() + auctionTime}
-	s.WaitForDone()
-	s.CurrentAuction.Lock()
-	s.CurrentAuction.IsDone = true
-	s.CurrentAuction.Unlock()
+	//start an auction every 15 seconds as long as there are no ongoing ones
+	for {
+		log.Println("Starting Auction, 60 seconds to bid")
+		s.CurrentAuction = Auction{TopBidder: "", HighestBid: 10, TimeLeft: time.Now().Unix() + auctionTime}
+		s.WaitForDone()
+		s.CurrentAuction.Lock()
+		s.CurrentAuction.IsDone = true
+		s.CurrentAuction.Unlock()
+
+		//wait for a while
+		log.Println("Starting Auction in 15 seconds.")
+		time.Sleep(time.Duration(15) * time.Second)
+	}
 }
 
 func (s *Server) StartProcess() {}
@@ -71,6 +100,7 @@ func (s *Server) WaitForDone() {
 
 // AuctionStream is the server-to-server connection stream.
 func (s *Server) AuctionStream(stream p.AuctionService_AuctionStreamServer) error {
+	var msg p.Result
 	for {
 		req, err := stream.Recv()
 		if err != nil {
@@ -78,16 +108,31 @@ func (s *Server) AuctionStream(stream p.AuctionService_AuctionStreamServer) erro
 		}
 		s.CurrentAuction.Lock()
 		if req.IsBid {
-			if s.CurrentAuction.HighestBid < *req.Price {
-				s.CurrentAuction.HighestBid = *req.Price //update highest bidder (max is int32 limit)
+			if s.CurrentAuction.HighestBid < req.Price {
+				s.CurrentAuction.HighestBid = req.Price //update highest bidder (max is int32 limit)
 			}
 		} else {
 			if s.CurrentAuction.IsDone {
 				//spit out winner
+				msg = p.Result{
+					Success:  true,
+					IsDone:   true,
+					UserID:   s.CurrentAuction.TopBidder,
+					Price:    s.CurrentAuction.HighestBid,
+					TimeLeft: 0,
+				}
+				stream.Send(&msg)
 			} else {
 				//spit out highest bid
+				msg = p.Result{
+					Success:  true,
+					UserID:   s.CurrentAuction.TopBidder,
+					Price:    s.CurrentAuction.HighestBid,
+					TimeLeft: s.CurrentAuction.TimeLeft,
+					IsDone:   false,
+				}
+				stream.Send(&msg)
 			}
-
 		}
 		s.CurrentAuction.Unlock()
 
